b695238c26fd8a9889d324b8fa73b646f93abe39
C4ArtZ <dev@c4artz.de>
2021-02-12 16:17:24 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM debian:latest
+
+RUN apt-get update && \
+	apt-get -y install --no-install-recommends screen curl jq unzip && \
+	rm -rf /var/lib/apt/lists/*
+
+ENV DATA_DIR="/serverdata"
+ENV SERVER_DIR="${DATA_DIR}/serverfiles"
+ENV RUNTIME="basicjre"
+ENV EXECUTABLE="server"
+ENV GAME_VERSION=""
+ENV GAME_PARAMETERS=""
+ENV GAME_PORT=25565
+ENV XMX=1024
+ENV XMS=1024
+ENV EXTRA_JVM_PARAMETERS=""
+ENV ACCEPT_EULA="false"
+ENV UMASK=000
+ENV UID=99
+ENV GID=100
+ENV USER="minecraft"
+ENV DATA_PERMISSION=770
+ENV MODLOADER="forge"
+
+ADD /scripts/ /opt/scripts/
+
+RUN mkdir $DATA_DIR && mkdir $SERVER_DIR && useradd -d $DATA_DIR -s /bin/bash $USER && chown -R $USER $DATA_DIR && ulimit -n 2048 && chmod -R 770 /opt/scripts/
+
+ENTRYPOINT ["/opt/scripts/start.sh"]
---
+++ scripts/start.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+echo "[SERVER] Checking if UID: ${UID} matches user..."
+usermod -u ${UID} ${USER}
+echo "[SERVER] Checking if GID: ${GID} matches user..."
+usermod -g ${GID} ${USER}
+echo "[SERVER] Setting umask to ${UMASK}..."
+umask ${UMASK}
+
+
+
+echo "---Starting...---"
+chown -R ${UID}:${GID} /opt/scripts
+chown -R ${UID}:${GID} ${DATA_DIR}
+
+term_handler() {
+	screenpid="$(su $USER -c "screen -list | grep "Detached" | grep "Minecraft" | cut -d '.' -f1")"
+	su $USER -c "screen -S Minecraft -X stuff 'stop^M'" >/dev/null
+	tail --pid="$(pidof java)" -f 2>/dev/null
+	exit 143;
+}
+
+trap 'kill ${!}; term_handler' SIGTERM
+
+echo "[SERVER] Checking for Modloader..."
+
+if [[${MODLOADER} -e "forge"]]
+then
+    echo "[SERVER] Modloader 'forge' was selected."
+    su ${USER} -c "/opt/scripts/start-forge.sh" &
+    killpid="$!"
+elif [[${MODLOADER} -e "fabric"]]
+then
+    echo "[SERVER] Modloader 'fabric' was selected."
+    su ${USER} -c "/opt/scripts/start-fabric.sh" &
+    killpid="$!"
